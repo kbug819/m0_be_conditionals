0ffae3ee4e03606525bff3925587fc8b7988df42
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-05-14 22:08:27 -0500

    Add mild coding

---
+++ code_challenges/mild.rb
@@ -0,0 +1,13 @@
+## Mild
+# Write a Ruby program that defines a variable 
+#that stores an Integer. The program should print out 
+#the String "even" if the Integer is even, and the 
+#String "odd" if the Integer is odd.
+
+num = -5
+
+if num.odd?
+    puts "odd"
+else
+    puts "even"
+end
