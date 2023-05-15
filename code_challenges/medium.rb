2056c26790eaee1fc0baf9fce62ee0c3c43398c8
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-05-14 22:21:56 -0500

    Add medium coding

--- code_challenges/medium.rb
+++ code_challenges/medium.rb
@@ -5,7 +5,26 @@
 #good_driving_record = true
 #age = 24
 
-#Write a program that evaluates the status of the variables above, and uses logic to print one of three possible messages:
-#- If the driving record is good and user is over 25 years old, they should get a discount on the car rental
-#- If the user either has a good record or is over 25 years old, they should pay full price
-#- If the user is not over 25 and has a bad driving record, they need to have someone else sign for the rental
+#Write a program that evaluates the status of the variables above, 
+#and uses logic to print one of three possible messages:
+
+#- If the driving record is good and user is over 25 years old, 
+#they should get a discount on the car rental
+
+#- If the user either has a good record or is over 25 years old, 
+#they should pay full price
+
+#- If the user is not over 25 and has a bad driving record, 
+#they need to have someone else sign for the rental
+
+good_driving_record = true
+age = 26
+
+if good_driving_record == true && age > 25
+    puts "You will get a discount on the car rental!"
+elsif good_driving_record == true || age > 25
+    puts "You will pay full price for your rental"
+else good_driving_record == false && age < 25
+    puts "You will need someone else to rent this car"
+
+end
